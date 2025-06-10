7df38da26562022ece095e69b618a2674e727be1
KyleStodola <airfoil.research@gmail.com>
2025-06-10 15:48:58 -0500

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,9 @@
+// File: Test.java
+
+public class Test {
+
+    // Main method - entry point of the program
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
